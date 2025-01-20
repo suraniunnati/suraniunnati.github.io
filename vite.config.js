85f41c346ce362405e64534d7bbd8a31a6d94225
suraniunnati <suraniunnati5@gmail.com>
2025-01-20 10:18:32 +0530

    portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: 'https://username.github.io/'
+   base: 'https://username.github.io/',
+   esbuild: {
+    loader: 'jsx', // Treat .js files as JSX
+    include: /src\/.*\.js$/, // Apply only to files in the src folder
+  },
 })
 
 
